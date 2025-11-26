faf549e1430a10d6e58d79de127e3b70ccbae3af
YUNJAEGOONER <coygyj@gmail.com>
2025-11-26 15:32:22 +0900

    Initial commit

---
+++ src/main/java/kspo/onfit/OnfitApplication.java
@@ -0,0 +1,13 @@
+package kspo.onfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnfitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnfitApplication.class, args);
+    }
+
+}
