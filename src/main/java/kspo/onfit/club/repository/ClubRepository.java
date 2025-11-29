862dd64767f8e8d0eafc6b2d65d86baa1874d7cc
YUNJAEGOONER <coygyj@gmail.com>
2025-11-29 12:10:27 +0900

    refactor: 동호회(Club) 도메인 삭제

--- src/main/java/kspo/onfit/club/repository/ClubRepository.java
+++
@@ -1,8 +0,0 @@
-package kspo.onfit.club.repository;
-
-import kspo.onfit.club.domain.Club;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClubRepository extends JpaRepository<Club, Long> {
-
-}
