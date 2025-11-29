66ce80521e6f7811e13b68815a8f79f202b3a4b8
YUNJAEGOONER <coygyj@gmail.com>
2025-11-29 23:21:31 +0900

    chroe: 불필요한 import문 삭제

--- src/main/java/kspo/onfit/imageFile/controller/S3Controller.java
+++ src/main/java/kspo/onfit/imageFile/controller/S3Controller.java
@@ -1,7 +1,6 @@
 package kspo.onfit.imageFile.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kspo.onfit.imageFile.dto.PreSignedResponseDto;
 import kspo.onfit.imageFile.service.S3Service;
